43b6c0f2134d691b188e68ddb2e09e1663ad7618
Shengyu Jin <jinshengyu1996@gmail.com>
2021-07-29 06:56:05 +0100

    third Rango says hey there partner --from kim

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner --from kim")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+def about(request):
+    return HttpResponse("Rango says here is the about page <a href='/rango/'>Index</a>")
